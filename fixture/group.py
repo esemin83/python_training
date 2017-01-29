762595730f789ed1c5a755cf768390d48349bc77
esemin83 <esemin83@gmail.com>
2017-01-29 19:46:50 +0400

    Удалил класс фикстуры Application_contact, собрал оба класса в Application.

--- fixture/group.py
+++ fixture/group.py
@@ -1,7 +1,7 @@
 
 
 class GroupHelper:
-
+    # groups metods
     def __init__(self, app):
         self.app = app
 
@@ -47,3 +47,47 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         # return to group page
         self.return_to_group_page()
+
+    # address metods
+    def add_new_address_form(self, address_data):
+        # add new contact
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("%s" % address_data.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("%s" % address_data.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("%s" % address_data.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("%s" % address_data.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("%s" % address_data.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("%s" % address_data.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("%s" % address_data.home_phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("%s" % address_data.mobile_phone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys("%s" % address_data.work_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("%s" % address_data.email_1)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys("%s" % address_data.email_2)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys("%s" % address_data.home_page)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-import pytest
+#import pytest
 from model.group_address import Address_data
-from fixture.application_contact import Application_contact
+#from fixture.application_contact import Application_contact
 
 
-@pytest.fixture(scope="session")
-def app(request):
-    fixture = Application_contact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+#@pytest.fixture(scope="session")
+#def app(request):
+#    fixture = Application_contact()
+#    request.addfinalizer(fixture.destroy)
+#    return fixture
 
 
 def test_test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.contact.add_new_address_form(Address_data(firstname="Jonny", middlename="Q", lastname="Cage", nickname="JQ", company="Andromeda", address="Moskow", home_phone="455-63-63",
+        app.group.add_new_address_form(Address_data(firstname="Jonny", middlename="Q", lastname="Cage", nickname="JQ", company="Andromeda", address="Moskow", home_phone="455-63-63",
                                   mobile_phone="+7-000-000-00-00", work_phone="456-85-85", email_1="fake_1@mail.ru", email_2="fake_2@mail.ru", home_page="www.page.ru"))
         app.session.logout()
