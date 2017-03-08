99c558d7dfffa7fc5661f74f03dbd131caafde21
esemin83 <esemin83@gmail.com>
2017-03-08 18:52:13 +0400

    Задание 22_1

--- test/test_to_test.py
+++ test/test_to_test.py
@@ -3,19 +3,23 @@ import random
 import mysql.connector
 import pymysql.cursors
 from model.group_address import Address_data
+from fixture.orm import ORMFixture
+from model.group import Group
 
 
 #connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
-#try:
-#    cursor = connection.cursor()
-#    cursor.execute("select * from group_list")
-#    for row in cursor.fetchall():
-#        print(row)
-#finally:
-#    connection.close()
+try:
+    l = db.get_contact_in_groups(Group(id="206"))
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass
 
 
-def test_2(app, db):
-    x = db.get_contact_list()
-    print(x)
+
+#def test_2(app, db):
+#    x = db.get_contact_list()
+#    print(x)
